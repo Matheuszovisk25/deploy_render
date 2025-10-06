ee4f6e2c53b9e509d87ed0bf27c05443bb2c0991
Matheus Pereira <152235011+Matheuszovisk25@users.noreply.github.com>
2025-10-06 18:40:05 -0300

    Update __all_models.py

--- models/__all_models.py
+++ models/__all_models.py
@@ -1,10 +1,12 @@
+# models/__all_models.py
 
-from sqlalchemy.orm import declarative_base
-Base = declarative_base()
+# Use SEMPRE o mesmo Base dos modelos:
+from core.settings import DBBaseModel as Base  # <- importantíssimo!
 
+# Importe todos os modelos para que registrem no metadata:
 from models.user_model import UserModel
 from models.producao_model import ProducaoModel
 from models.processamento_model import ProcessamentoModel
-from models.comercializacao_model import  ComercializacaooModel
+from models.comercializacao_model import ComercializacaoModel  # confira o nome da classe
 from models.exportacao_model import ExportacaoModel
 from models.importacao_model import ImportacaoModel
