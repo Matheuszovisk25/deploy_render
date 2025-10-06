23dda7bf3ac8e75bcbbeba68ae8858673ed5abd5
Matheus Pereira <152235011+Matheuszovisk25@users.noreply.github.com>
2025-10-06 18:08:23 -0300

    Update database.py

--- core/database.py
+++ core/database.py
@@ -1,10 +1,17 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-from core.settings import settings
+from core.settings import settings, DBBaseModel
 
 engine = create_async_engine(settings.DB_URL, echo=True)
-SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_session() -> AsyncSession:
     async with SessionLocal() as session:
         yield session
+
+# 👉 rode isso uma vez na inicialização
+async def init_db():
+    # importa todos os models para registrar as tabelas no metadata
+    import models.__all_models  # garante que UserModel e cia. estejam carregados
+    async with engine.begin() as conn:
+        await conn.run_sync(DBBaseModel.metadata.create_all)
