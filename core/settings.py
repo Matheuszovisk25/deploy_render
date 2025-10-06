16fc60e9d08feaf89e2ba2e09812f2864f2dd6a4
Matheus Pereira <152235011+Matheuszovisk25@users.noreply.github.com>
2025-10-06 18:57:25 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     #DB_URL: str = "postgresql+asyncpg://postgres:admin@localhost:5432/techchallenge_db"
     DB_URL: str = "postgresql+asyncpg://admin_fkij_user:A8ygffEUSBFsWCmdnvAB4i6xw0PdDt69@dpg-d3i2c60gjchc73asutpg-a:5432/admin_fkij?ssl=require"
-    JWT_SECRET_KEY: str = 'P7CpvrzKq7FH3xO5wtnNxJ84q2wGL28hNSS1pep2wrk'
+    JWT_SECRET_KEY: str = '75d13f95c062c9ab0f1a59ea5959df76'
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 1 semana
 
