cb8c909f9661a4f60cadf1ed66d7b44f6cf9e67c
Matheus Pereira <152235011+Matheuszovisk25@users.noreply.github.com>
2025-10-07 16:02:44 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -6,7 +6,7 @@ DBBaseModel = declarative_base()
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     #DB_URL: str = "postgresql+asyncpg://admin:mTeHvN2zSYQWgmqlNsCOuSoxKqtYzAGL@dpg-d0ua86idbo4c73ap46q0-a/db_techchalenge"
-    DB_URL: str = "postgresql+asyncpg://db_certo:xLkY0nQ26QTGvEUA2nGsw98rWrgeMETB@dpg-d3i88k2dbo4c73fgrf3g-a.oregon-postgres.render.com/db_certo_gddh"
+    DB_URL: str = "postgresql+asyncpg://db_certo:xLkY0nQ26QTGvEUA2nGsw98rWrgeMETB@dpg-d3i88k2dbo4c73fgrf3g-a.oregon-postgres.render.com/db_certo_gddh?sslmode=require"
     JWT_SECRET_KEY: str = 'xLkY0nQ26QTGvEUA2nGsw98rWrgeMETB'
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 7  # 1 semana
